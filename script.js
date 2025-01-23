455ff362056ba243ac5536ebbedc810f668ec3df
Gerardo Chavez <159754943+Gera1200@users.noreply.github.com>
2025-01-23 11:36:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const toggleMenuButton = document.getElementById("toggle-menu");
 const sidebar = document.getElementById("sidebar");
-const container = document.querySelector(".container");
+const mainContent = document.querySelector("main");
 
 // Mostrar/ocultar el menú lateral
 toggleMenuButton.addEventListener("click", () => {
     sidebar.classList.toggle("hidden");
-    // Ajustar el margen del contenido dependiendo del estado del menú
     if (sidebar.classList.contains("hidden")) {
-        container.style.marginLeft = "0";
+        mainContent.style.marginLeft = "0";
     } else {
-        container.style.marginLeft = "250px";
+        mainContent.style.marginLeft = "250px";
     }
 });
 
@@ -30,16 +29,5 @@ menuButtons.forEach((button) => {
             sections.forEach((section) => section.classList.remove("active"));
             targetSection.classList.add("active");
         }
-    });
-});
-
-// Mostrar/ocultar descripción de carreras
-const carreraButtons = document.querySelectorAll(".carrera-btn");
-
-carreraButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const description = button.nextElementSibling;
-        description.style.display =
-            description.style.display === "block" ? "none" : "block";
     });
 });
